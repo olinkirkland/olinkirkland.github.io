95d5a8035b22049c47b0dde21b3e4d8e93b1b95c
Olin Kirkland <olin.kirkland@beg.de>
2025-08-12 16:58:32 +0200

    Update footer with project section & links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,16 +4,97 @@
             <div class="footer-content">
                 <h3>About Me</h3>
                 <p>
-                    Living and working in Albany, New York. I write full-stack
-                    code for web-apps and games.
+                    I write full-stack code for web-apps and games in Albany,
+                    New York.
                 </p>
-
-                
 
                 <Button class="dark" @click="$emit('scrollToTop')">
                     <i class="icon fas fa-arrow-up"></i>
                     <span>Back to top</span>
                 </Button>
+            </div>
+
+            <!-- <div class="footer-content"></div> -->
+            <div class="footer-content">
+                <h3>Projects</h3>
+                <ul>
+                    <li>
+                        <a
+                            class="link"
+                            href="https://scoundry.com"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Scoundry
+                        </a>
+                    </li>
+
+                    <li>
+                        <a
+                            class="link"
+                            href="https://olinkirk.land/grimoire"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Grimoire
+                        </a>
+                    </li>
+
+                    <li>
+                        <a
+                            class="link"
+                            href="https://olinkirk.land/lootdragon"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Lootdragon
+                        </a>
+                    </li>
+
+                    <li>
+                        <a
+                            class="link"
+                            href="https://olinkirk.land/scribbles"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Scribbles
+                        </a>
+                    </li>
+
+                    <li>
+                        <a
+                            class="link"
+                            href="https://github.com/olinkirkland/realms"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Realms
+                        </a>
+                    </li>
+
+                    <li>
+                        <a
+                            class="link"
+                            href="https://github.com/olinkirkland/online-in-the-dark"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Online in the Dark
+                        </a>
+                    </li>
+
+                    <li>
+                        <a
+                            class="link"
+                            href="https://olinkirk.land/realm-writer"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >
+                            Realm Writer
+                        </a>
+                    </li>
+                </ul>
             </div>
 
             <div class="footer-content">
@@ -48,8 +129,11 @@
     <div class="copyright">
         <section>
             <p>
-                Copyright &copy; {{ new Date().getFullYear() }} Olin Kirkland.
-                All Rights Reserved.
+                <span
+                    >Copyright &copy; {{ new Date().getFullYear() }} Olin
+                    Kirkland.</span
+                >
+                <span>All Rights Reserved.</span>
             </p>
         </section>
     </div>
@@ -59,7 +143,7 @@
 defineEmits<{ (e: 'scrollToTop'): void }>();
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 footer {
     background-color: var(--surface-inverted);
 }
@@ -67,7 +151,7 @@ footer {
 footer section {
     width: 100%;
     display: grid;
-    grid-template-columns: repeat(auto-fit, minmax(20rem, 1fr));
+    grid-template-columns: repeat(3, 1fr);
     gap: 2rem;
     padding: 2rem;
 }
@@ -79,11 +163,24 @@ footer section {
     color: var(--surface);
 }
 
+.footer-content ul {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    gap: 0.8rem;
+}
+
 .copyright {
     background-color: var(--accent-1);
     padding: 1rem;
-    > p {
-        font-size: 1.2rem;
+
+    section {
+        padding: 0;
+
+        > p {
+            display: flex;
+            justify-content: center;
+            gap: 0.4rem;
+        }
     }
 }
 
@@ -91,10 +188,21 @@ a i {
     font-size: 2.4rem;
     color: var(--surface);
 }
+</style>
 
-.copyright section {
-    padding: 0;
-    display: flex;
-    justify-content: center;
+<!-- Mobile queries -->
+<style lang="scss" scoped>
+@media (max-width: 768px) {
+    footer section {
+        grid-template-columns: 1fr;
+        gap: 4rem;
+    }
+
+    .copyright section {
+        p {
+            flex-direction: column;
+            align-items: center;
+        }
+    }
 }
 </style>
