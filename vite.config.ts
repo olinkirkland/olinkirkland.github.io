507ef804c7bd5f68e7f943e0c6b208dd3a0e2d1b
Olin Kirkland <olin.kirkland@beg.de>
2025-08-15 16:57:58 +0200

    base url fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-    base: mode === 'development' ? '/' : '/site-2025/',
     plugins: [vue()]
 }));
